b218f553eee1ebf8bf78c444408b7a9609fd7567
Mvlik6 <mdv6669@outlook.com>
2023-01-18 19:25:28 +0300

    Updates

--- script.js
+++ script.js
@@ -31,35 +31,3 @@ function toggler(){
     document.documentElement.classList.toggle('dark');
     
 }
-
-// Get all sections that have an ID defined
-const sections = document.querySelectorAll("#main,#about,#projects,#contact");
-
-// Add an event listener listening for scroll
-window.addEventListener("scroll", navHighlighter);
-
-function navHighlighter() {
-  
-  // Get current scroll position
-  let scrollY = window.pageYOffset;
-  
-  // Now we loop through sections to get height, top and ID values for each
-  sections.forEach(current => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 100;
-    sectionId = current.getAttribute("id");
-    
-    /*
-    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-    */
-    if (
-      scrollY > sectionTop &&
-      scrollY <= sectionTop + sectionHeight
-    ){
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("animate-ping");
-    } else {
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("animate-ping");
-    }
-  });
-}
